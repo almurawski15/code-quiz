1d9a874d2b33603774644e3437f31e2c7c6d55ca
Alexandra Murawski <almurawski15@gmail.com>
2021-02-20 21:18:11 -0500

    finalized .js, .html, and .css files, added snapshots and wrote out README

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # code-quiz
+
+# Unit 4 Web APIs: Code Quiz Assignment
+
+## Description
+This project is a timed coding quiz with multiple-choice questions featuring dynamically updated HTML and CSS powered by JavaScript code.
+
+When prompted the quiz should meet the following criteria:
+
+1. On click of start button a timer of 75 seconds should start
+2. By clicking answer choice, questions should loop to next prompt question
+3. When a question is answered incorrectly time is subtracted from the timer, vice versa, if answered correctly points will be added to the highest score
+4. When the game is over, user should be able to save intials and score 
+
+## Screenshots
+
+The following images shows the web application's appearance and functionality:
+
+![code-quiz](assets/quiz_snip_1.PNG)
+
+![code-quiz](assets/quiz_snip_2.PNG)
+
+![code-quiz](assets/quiz_snip_3.PNG)
+
+## Deployed Project Location: 
+* Page published at: https://almurawski15.github.io/password-generator/
+
+## Creator Comments
+
+This particular assignment was really difficult. I was able to get most of the code working, left a few glitches including the timer that doesn't stop. With more time and a recent understanding of bootstrap and jquery I would of liked to spend more time on the appearance and functionality of the Javascript! 
--- script.js
+++ script.js
@@ -1,17 +1,149 @@
+//Creates array of questions, choices and correct answers
 var questions = [
-    {context: "Test?",
-    choices: ["a( )", "b( )", "c( )"],
-    answer: "a( )"
+    {context: "Which of the following is an advantage of using JavaScript?",
+    choices: ["Less server interaction", "Increased interactivity", "All of the above."],
+    answer: "All of the above."
 
     },
+    {context: "Which of the following function of Array object adds and/or removes elements from an array?",
+    choices: ["toSource()", "splice()", "sort()"],
+    answer: "splice()"
 
+    },
+    {context: "Which of the following function of Array object returns true if every element in this array satisfies the provided testing function?",
+    choices: ["concat()", "push()", "every()"],
+    answer: "every()"
 
+    },
+    {context: "Which of the following function of String object returns a string representing the specified object?",
+    choices: ["toLocalUpperCase()", "toUpperCase()", "toString()"],
+    answer: "toString()"
 
+    },
+    {context: "Which of the following function of String object causes a string to be italic, as if it were in an <i> tag?",
+    choices: ["fontcolor()", "fontsize()", "italics()"],
+    answer: "italics()"
 
-
-
-
-
-
-
+    }
 ]
+
+//Sets the numerical variables of the functions for both the timer and highscore 
+var score = 0;
+var currentQuestion = -1;
+var timeLeft = 0;
+var timer;
+
+//On 'click' of start button, timer will begin countdown at 75 seconds 
+function start() {
+
+    timeLeft = 75;
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    timer = setInterval(function() {
+        timeLeft--;
+        document.getElementById("timeLeft").innerHTML = timeLeft;
+        //If at any point timer reaches 0, the game will end 
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            endGame(); 
+        }
+    }, 1000);
+
+    next();
+}
+
+//timer stops to end the game 
+function endGame() {
+    clearInterval(timer);
+
+    var quizContent = `
+    <h2>Game over!</h2>
+    <h3>You got a ` + score +  ` /100!</h3>
+    <h3>That means you got ` + score / 20 +  ` questions correct!</h3>
+    <input type="text" id="name" placeholder="Initials"> 
+    <button onclick="setScore()">Set score!</button>`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+//stores the user's score on local storage
+function setScore() {
+    localStorage.setItem("highscore", score);
+    localStorage.setItem("highscoreName",  document.getElementById('name').value);
+    getScore();
+}
+
+
+function getScore() {
+    var quizContent = `
+    <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+    <h1>` + localStorage.getItem("highscore") + `</h1><br> 
+    
+    <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
+    
+    `;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+//clears the score name and value in the local storage if the user selects 'clear score'
+function clearScore() {
+    localStorage.setItem("highscore", "");
+    localStorage.setItem("highscoreName",  "");
+
+    resetGame();
+}
+
+//resets the game 
+function resetGame() {
+    clearInterval(timer);
+    score = 0;
+    currentQuestion = -1;
+    timeLeft = 0;
+    timer = null;
+
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    var quizContent = `
+    <h1>
+        JavaScript Quiz!
+    </h1>
+    <h3>
+        Click to play!   
+    </h3>
+    <button onclick="start()">Start!</button>`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+//deduct 15seconds from the timer if user chooses an incorrect answer
+function incorrect() {
+    timeLeft -= 15; 
+    next();
+}
+
+//Creates loop of questions  
+function next() {
+    currentQuestion++;
+
+    if (currentQuestion > questions.length - 1) {
+        endGame();
+        return;
+    }
+
+    var quizContent = "<h2>" + questions[currentQuestion].context + "</h2>"
+
+    for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {
+        var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
+        buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[buttonLoop]);
+        if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {
+            buttonCode = buttonCode.replace("[ANS]", "correct()");
+        } else {
+            buttonCode = buttonCode.replace("[ANS]", "incorrect()");
+        }
+        quizContent += buttonCode
+    }
+
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
